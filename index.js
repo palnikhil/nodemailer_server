9847c24b1086cf8d3ae4a3d17da89fb3030fd991
unknown <nikhilpal9111@gmail.com>
2023-07-02 06:08:54 +0530

    changed server.js 7

--- index.js
+++ index.js
@@ -49,21 +49,21 @@ async function saveCredentials(client) {
 /**
  * Load or request or authorization to call APIs.
  *
- */
-async function authorize() {
-  let client = await loadSavedCredentialsIfExist();
-  if (client) {
-    return client;
-  }
-  client = await authenticate({
-    scopes: SCOPES,
-    keyfilePath: CREDENTIALS_PATH,
-  });
-  if (client.credentials) {
-    await saveCredentials(client);
-  }
-  return client;
-}
+//  */
+// async function authorize() {
+//   let client = await loadSavedCredentialsIfExist();
+//   if (client) {
+//     return client;
+//   }
+//   client = await authenticate({
+//     scopes: SCOPES,
+//     keyfilePath: CREDENTIALS_PATH,
+//   });
+//   if (client.credentials) {
+//     await saveCredentials(client);
+//   }
+//   return client;
+// }
 
 /**
  * Lists the labels in the user's account.
